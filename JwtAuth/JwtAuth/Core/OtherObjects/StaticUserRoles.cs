e7c923c937da02f5695ee6c7d9879c23f7de2791
aykanatcanberk <kayra.aykanat@gmail.com>
2023-09-04 14:26:29 +0300

    Dtos

---
+++ JwtAuth/JwtAuth/Core/OtherObjects/StaticUserRoles.cs
@@ -0,0 +1,10 @@
+﻿namespace JwtAuth.Core.OtherObjects
+{
+    public static class StaticUserRoles
+    {
+        public const string Owner = "Owner";
+        public const string Admin = "Admin";
+        public const string User = "User";
+
+    }
+}
